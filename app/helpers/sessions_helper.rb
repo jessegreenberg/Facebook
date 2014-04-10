0b5b2d5e345df5af99cf13e64e2b8fe3e85db6f0
Jesse Greenberg <jesse.greenberg@colorado.edu>
2014-04-10 15:35:10 -0300

    Sign In Mods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,6 +17,6 @@ module SessionsHelper
 
   def current_user
     remember_token = User.hash(cookies[:remember_token])
-    @current_user ||= User.find_by(remeber_token: remember_token)
+    @current_user ||= User.find_by(remember_token: remember_token)
   end
 end
