401c47f85a38f64abb69c3927c0f35cc30f25e06
dofi <douglas.fitzpatrick@colorado.edu>
2014-04-13 20:31:55 -0300

    Capybara issues

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -34,7 +34,7 @@ describe "Authentication" do
 			it { should have_link('Profile', href: user_path(user)) }
 			it { should have_link('Settings', href: edit_user_path(user)) }
 			it { should have_link('Sign Out', href: signout_path) }
-			it { should_not have_link('Sign In', href: signin_path) }
+			it { should_not have_link('Sign In', href: root_path) }
 
 			describe "followed by signout" do
 				before { click_link "Sign Out" }
