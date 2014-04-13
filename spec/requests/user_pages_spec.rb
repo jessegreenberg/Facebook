8963df5345b17feb9d77fe711a6dea157d7e3586
dofi <douglas.fitzpatrick@colorado.edu>
2014-04-13 19:08:22 -0300

    Edit preference page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -40,6 +40,20 @@ describe "User Pages" do
 		it { should have_title("Facebook | #{user.name}") }
 	end
 	
+	describe "edit" do
+    		let(:user) { FactoryGirl.create(:user) }
+    		before { visit edit_user_path(user) }
 
+    	describe "page" do
+      		it { should have_content("Update your profile") }
+      		it { should have_title("Facebook | Preferences") }
+    	end
+
+    	describe "with invalid information" do
+      		before { click_button "Save changes" }
+
+      		it { should have_content('error') }
+    	end
+  end
 end
 	
