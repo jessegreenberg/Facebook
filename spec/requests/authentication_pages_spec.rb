976c0bd18eeef28520588b20aded4843a23767ed
dofi <douglas.fitzpatrick@colorado.edu>
2014-04-13 19:30:16 -0300

    More settings stuff

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -32,6 +32,7 @@ describe "Authentication" do
 			# link location.
 			it { should have_title("Facebook | Newsfeed") }
 			it { should have_link('Profile', href: user_path(user)) }
+			it { should have_link('Settings', href: edit_user_path(user)) }
 			it { should have_link('Sign Out', href: signout_path) }
 			it { should_not have_link('Sign In', href: signin_path) }
 
