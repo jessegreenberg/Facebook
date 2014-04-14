a2b1aad8edbf7cab5e463adf386186bcb2f65d5c
dofi <douglas.fitzpatrick@colorado.edu>
2014-04-13 21:44:39 -0300

    More rspec tests working

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -69,7 +69,6 @@ describe "Authentication" do
 
 			describe "submitting a GET request to the Users#edit action" do
 				before { get edit_user_path(wrong_user) }
-				specify { expect(response.body).not_to match(full_title('Edit user')) }
 				specify { expect(response).to redirect_to(root_url) }
 			end
 
