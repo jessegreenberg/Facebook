61f3ba1aa31894f12a21854f8ca0c142ec337234
Jesse Greenberg <jesse.greenberg@colorado.edu>
2014-04-15 00:54:01 -0300

    Paginated Friend List

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
 	factory :user do
-		name "Jesse Greenberg"
-		email "jesse.greenberg@colorado.edu"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com" }
 		password "foobar"
 		password_confirmation "foobar"
 	end
